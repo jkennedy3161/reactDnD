533b5337cfcd6e736609d947a7af32e13ffed98d
jkennedy3161 <kennedyjt88@gmail.com>
2016-11-26 23:13:00 -0800

    update for production deployment to display on herokuapp

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 var express = require('express');
+var parser = require('body-parser');
 var app = express();
+
+var morgan = require('morgan');
+var path = require('path');
+
+app.use(morgan('dev'));
+app.use(parser.urlencoded({extended: true}));
+app.use(parser.json());
+app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 8080;
 
-app.listen(port);
+
+app.listen(port, () => {
+  console.info('server connected at http://localhost:8080');
+});
+
+module.exports = app;
