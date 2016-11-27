1e44559fe0c26ba879954089caa3e0cf898ef5b3
jkennedy3161 <kennedyjt88@gmail.com>
2016-11-26 17:17:17 -0800

    add logic for updating ticket state when square is clicked

--- src/components/game.js
+++ src/components/game.js
@@ -1,6 +1,20 @@
-export function observe(receive) {
-  setInterval(() => receive([
-    Math.floor(Math.random() * 8),
-    Math.floor(Math.random() * 8)
-  ]), 500);
+let ticketPosition = [0, 0];
+let observer = null;
+
+var emitChange = () => {
+  observer(ticketPosition);
+};
+
+export function observe(o) {
+  if (observer) {
+    throw new Error('Multiple observers not implemented.');
+  }
+
+  observer = o;
+  emitChange();
+}
+
+export function moveTicket(toX, toY) {
+  ticketPosition = [toX, toY];
+  emitChange();
 }
